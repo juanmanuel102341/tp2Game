c14b3ea04b6bcc3314de6d629305e6b6cc4765c4
juanmanuel102341 <jms24@live.com.ar>
2018-04-25 09:03:24 -0300

    creacion de ventana 32 y 64 bits

---
+++ SalaTp2/SalaTp2/Source.cpp
@@ -0,0 +1,12 @@
+//#include"Window.hpp"
+#include<SFML/Window.hpp>
+#include<iostream>
+using namespace std;
+int main() {
+	int n;
+	sf::Window window(sf::VideoMode(800, 600), "My window");
+
+	std::cin>> n;
+	return 0;
+
+}
